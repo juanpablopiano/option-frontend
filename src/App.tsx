374ab312fda18446e8de2392e561183fe908cc14
Juan Gonzalez <jgu1@hotmail.com>
2021-12-26 22:18:16 -0300

    Setup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import CardList from './components/CardList';
+import axios from 'axios';
+import { Divider, Spin, Layout, Typography } from 'antd';
+const { Header, Content } = Layout;
+const { Title } = Typography;
 
-function App() {
-  return (
-    <div className="App">
-      <div>App</div>
-    </div>
-  );
-}
+const App: React.FC = () => {
+	const [keyword, setKeyword] = useState('');
+	const [videos, setVideos] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	async function getData(keyword: string) {
+		try {
+			setLoading(true);
+			const result = await axios.get(
+				`${process.env.REACT_APP_API_URL}/youtube/${keyword}`
+			);
+			setVideos(result.data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		if (keyword) {
+			getData(keyword);
+		}
+	}, [keyword]);
+
+	const handleClick = useCallback(
+		(keyword: string) => {
+			setKeyword(keyword);
+		},
+		[setKeyword]
+	);
+
+	return (
+		<Layout className='App'>
+			<Header
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<Title
+					level={2}
+					style={{
+						color: '#fff',
+						marginBottom: '0',
+					}}
+				>
+					YouTube Search
+				</Title>
+			</Header>
+			<Content>
+				<SearchBar onClick={handleClick} loading={loading} />
+				<Divider />
+				{loading ? <Spin /> : <CardList videos={videos} />}
+			</Content>
+		</Layout>
+	);
+};
 
 export default App;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Col, Input, Row } from 'antd';
+import React from 'react'
+import { Col, Input, Row } from 'antd'
 const { Search } = Input;
 
 interface SearchBarProps {
+    onClick: (keyword: string) => void;
     loading: boolean;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({loading,...props} ) => {
+const SearchBar: React.FC<SearchBarProps> = ({onClick, loading,...props} ) => {
     return (
         <Row justify='center' style={{
             marginTop: '2rem',
         }}>
             <Col span={12}>
-            <Search placeholder="Escriba el video que desea buscar" enterButton="Buscar" size="large" loading={loading} onSearch={() => console.log('clicked')} {...props}  />
+            <Search placeholder="Escriba el video que desea buscar" enterButton="Buscar" size="large" loading={loading} onSearch={onClick} {...props}  />
             </Col>
         </Row>
     )
 }
+
+export default SearchBar;
